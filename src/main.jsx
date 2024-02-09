4f7f5610ad66b5f10ba22431ebef2ed1611889e0
REFATBHUYAN <refatbubt@gmail.com>
2024-02-10 01:43:20 +0600

    add google analytics data

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import router from "./routers/Router.jsx";
 import { RouterProvider } from "react-router-dom";
 import ThemeProvider from "./components/ThemeProvider";
 import { HelmetProvider } from "react-helmet-async";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-6D9WCFRD89");
+
+ReactGA.send({ hitType: "pageview", page: "/" });
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
