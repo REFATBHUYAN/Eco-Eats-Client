388e080d0744a8f65b727b490a6986b19865ca60
Lava <bm.lava@gmail.com>
2024-02-11 09:51:42 -0500

    review star

--- src/pages/home/Review.jsx
+++ src/pages/home/Review.jsx
@@ -216,7 +216,31 @@ const Review = () => {
                 <SwiperSlide key={review.id}>
                   <div className="group cursor-pointer">
                     <a href={review.url} target="_blank">
-                      <div className=" h-60 w-full bg-slate-50 rounded-lg p-9 group-hover:bg-slate-100 ease-in duration-75">
+                      <div className="h-72 w-full bg-slate-50 rounded-lg p-9 group-hover:bg-slate-100 ease-in duration-75">
+                        <div className="flex gap-1 mb-4">
+                          {[1, 2, 3, 4, 5].map((item) => (
+                            
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              class="icon icon-tabler icon-tabler-star-filled text-amber-400"
+                              width="16"
+                              height="16"
+                              viewBox="0 0 24 24"
+                              stroke-width="2"
+                              stroke="currentColor"
+                              fill="none"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            >
+                              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                              <path
+                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
+                                stroke-width="0"
+                                fill="currentColor"
+                              />
+                            </svg>
+                          ))}
+                        </div>
                         <p className="text-slate-400 mb-8 line-clamp-3 font-light">
                           {review.description}
                         </p>
